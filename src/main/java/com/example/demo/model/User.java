e20969b022c5d61bf572a4d0b2dfc97b3fd6bcab
wangping <wangping222@qbitnetwork.com>
2025-07-16 13:46:08 +0800

    init

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import com.mybatisflex.annotation.Id;
+import com.mybatisflex.annotation.Table;
+import com.mybatisflex.annotation.Column;
+import lombok.Data;
+
+@Data
+@Table("tb_user")
+public class User {
+    /**
+     * 用户ID
+     */
+    @Id
+    private Long id;
+
+    /**
+     * 用户名称
+     */
+    @Column("user_name")
+    private String username;
+
+    /**
+     * 用户邮箱
+     */
+    @Column("email_address")
+    private String email;
+
+}
