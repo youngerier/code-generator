c20e816d751e225b3fb34426dd3b543afaf37a0d
wangping <wangping222@qbitnetwork.com>
2025-07-23 17:00:11 +0800

    init

--- src/main/java/com/example/demo/codegen/template/ServiceCodeTemplate.java
+++ src/main/java/com/example/demo/codegen/template/ServiceCodeTemplate.java
@@ -1,104 +1,86 @@
 package com.example.demo.codegen.template;
 
-import com.example.demo.codegen.config.CodeGenConfig;
-import com.example.demo.codegen.core.EntityMetadata;
-import com.example.demo.template.TemplateUtils;
+
+import com.github.javaparser.ast.body.FieldDeclaration;
 import com.mybatisflex.core.paginate.Page;
 import com.squareup.javapoet.*;
 
 import javax.lang.model.element.Modifier;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import com.example.demo.codegen.core.TemplateUtils;
 
-/**
- * Service接口代码模板
- */
-public class ServiceCodeTemplate implements CodeTemplate {
-    
-    @Override
-    public void generate(EntityMetadata metadata, CodeGenConfig config) throws IOException {
-        TypeName entityType = ClassName.get(metadata.getPackageName(), metadata.getClassName());
-        TypeName idTypeName = TemplateUtils.resolveTypeName(metadata.getIdType());
-        ClassName queryType = ClassName.get(config.getFullPackage("dto"), metadata.getClassName() + "Query");
+public class ServiceCodeTemplate {
+
+    private static final Path OUTPUT_DIR = Paths.get("src/main/java");
+
+    public static void generate(String entityName, String packageName, String idType, String classJavadoc, List<FieldDeclaration> fields) throws IOException {
+        TypeName entityType = ClassName.get(packageName + ".dal.entity", entityName);
+        TypeName idTypeName = TemplateUtils.resolveTypeName(idType);
+        ClassName queryType = ClassName.get(packageName + ".dto", entityName + "Query");
         ClassName pageType = ClassName.get(Page.class);
         ParameterizedTypeName listType = ParameterizedTypeName.get(ClassName.get(List.class), entityType);
-        
-        TypeSpec.Builder serviceBuilder = TypeSpec.interfaceBuilder(metadata.getClassName() + "Service")
-                .addModifiers(Modifier.PUBLIC)
-                .addJavadoc(metadata.getClassJavadoc() + "\nService interface for " + metadata.getClassName() + "\n");
-        
-        // 基础CRUD方法
-        serviceBuilder.addMethod(createSelectByIdMethod(entityType, idTypeName, metadata.getClassName()));
-        serviceBuilder.addMethod(createInsertMethod(entityType, metadata.getClassName()));
-        serviceBuilder.addMethod(createUpdateByIdMethod(entityType, metadata.getClassName()));
-        serviceBuilder.addMethod(createDeleteByIdMethod(idTypeName, metadata.getClassName()));
-        serviceBuilder.addMethod(createSelectListMethod(listType, queryType, metadata.getClassName()));
-        serviceBuilder.addMethod(createSelectPageMethod(pageType, entityType, queryType, metadata.getClassName()));
-        
-        JavaFile javaFile = JavaFile.builder(config.getFullPackage("service"), serviceBuilder.build())
-                .build();
-        
-        javaFile.writeTo(config.getOutputDir());
-    }
-    
-    private MethodSpec createSelectByIdMethod(TypeName entityType, TypeName idTypeName, String entityName) {
-        return MethodSpec.methodBuilder("selectById")
+
+        MethodSpec selectById = MethodSpec.methodBuilder("selectById")
                 .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
                 .returns(entityType)
                 .addParameter(idTypeName, "id")
-                .addJavadoc("根据ID查询$L\n@param id $L的ID\n@return $L实体\n", entityName, entityName, entityName)
+                .addJavadoc("Selects a " + entityName + " by ID.\n@param id the ID of the " + entityName + "\n@return the " + entityName + " entity\n")
                 .build();
-    }
-    
-    private MethodSpec createInsertMethod(TypeName entityType, String entityName) {
-        return MethodSpec.methodBuilder("insert")
+
+        MethodSpec insert = MethodSpec.methodBuilder("insert")
                 .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
                 .returns(TypeName.VOID)
                 .addParameter(entityType, "entity")
-                .addJavadoc("新增$L\n@param entity 要新增的$L\n", entityName, entityName)
+                .addJavadoc("Inserts a new " + entityName + ".\n@param entity the " + entityName + " to insert\n")
                 .build();
-    }
-    
-    private MethodSpec createUpdateByIdMethod(TypeName entityType, String entityName) {
-        return MethodSpec.methodBuilder("updateById")
+
+        MethodSpec updateById = MethodSpec.methodBuilder("updateById")
                 .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
                 .returns(TypeName.VOID)
                 .addParameter(entityType, "entity")
-                .addJavadoc("根据ID更新$L\n@param entity 要更新的$L\n", entityName, entityName)
+                .addJavadoc("Updates a " + entityName + " by ID.\n@param entity the " + entityName + " to update\n")
                 .build();
-    }
-    
-    private MethodSpec createDeleteByIdMethod(TypeName idTypeName, String entityName) {
-        return MethodSpec.methodBuilder("deleteById")
+
+        MethodSpec deleteById = MethodSpec.methodBuilder("deleteById")
                 .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
                 .returns(TypeName.VOID)
                 .addParameter(idTypeName, "id")
-                .addJavadoc("根据ID删除$L\n@param id 要删除的$L的ID\n", entityName, entityName)
+                .addJavadoc("Deletes a " + entityName + " by ID.\n@param id the ID of the " + entityName + " to delete\n")
                 .build();
-    }
-    
-    private MethodSpec createSelectListMethod(ParameterizedTypeName listType, ClassName queryType, String entityName) {
-        return MethodSpec.methodBuilder("selectList")
+
+        MethodSpec selectList = MethodSpec.methodBuilder("selectList")
                 .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
                 .returns(listType)
                 .addParameter(queryType, "query")
-                .addJavadoc("根据条件查询$L列表\n@param query 查询条件\n@return $L实体列表\n", entityName, entityName)
+                .addJavadoc("Queries a list of " + entityName + " based on conditions, excluding deleted records.\n@param query the query conditions\n@return the list of " + entityName + " entities\n")
                 .build();
-    }
-    
-    private MethodSpec createSelectPageMethod(ClassName pageType, TypeName entityType, ClassName queryType, String entityName) {
-        return MethodSpec.methodBuilder("selectPage")
+
+        MethodSpec selectPage = MethodSpec.methodBuilder("selectPage")
                 .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
                 .returns(ParameterizedTypeName.get(pageType, entityType))
                 .addParameter(TypeName.INT, "pageNumber")
                 .addParameter(TypeName.INT, "pageSize")
                 .addParameter(queryType, "query")
-                .addJavadoc("根据条件分页查询$L\n@param pageNumber 页码\n@param pageSize 页大小\n@param query 查询条件\n@return $L分页结果\n", entityName, entityName)
+                .addJavadoc("Queries a paginated list of " + entityName + " based on conditions, excluding deleted records.\n@param pageNumber the page number\n@param pageSize the page size\n@param query the query conditions\n@return the paginated list of " + entityName + " entities\n")
                 .build();
-    }
-    
-    @Override
-    public String getTemplateName() {
-        return "service";
+
+        TypeSpec service = TypeSpec.interfaceBuilder(entityName + "Service")
+                .addModifiers(Modifier.PUBLIC)
+                .addJavadoc(classJavadoc + "\nService interface for " + entityName + "\n")
+                .addMethod(selectById)
+                .addMethod(insert)
+                .addMethod(updateById)
+                .addMethod(deleteById)
+                .addMethod(selectList)
+                .addMethod(selectPage)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder(packageName + ".service", service)
+                .build();
+
+        javaFile.writeTo(OUTPUT_DIR);
     }
 }
